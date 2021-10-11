2e4011d4d24270d9501cbf91915649b7b1ff6b5d
pandurisathwik <74193944+pandurisathwik@users.noreply.github.com>
2021-10-11 15:29:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,47 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+RightWristX = 0;
+RightWristY = 0;
 
-function preload(){
+function preload() {
     song = loadSound("music.mp3");
 }
 
-function setup(){
-    canvas = createCanvas(600,500);
+function setup() {
+    canvas = createCanvas(600, 500);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
 }
 
-function draw(){
-    image(video,0,0,600,500);
+function modelLoaded() {
+    console.log("PoseNet is Initialized");
 }
-function play(){
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        RightWristX = results[0].pose.rightWrist.x;
+        RightWristY = results[0].pose.rightWrist.y;
+        console.log("RightWristX = " + RightWristX + "RightWristY = " + RightWristY);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function play() {
     song.play();
+    song.setVolume(0.1);
+    song.rate(2.0);
 }
